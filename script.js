555730660208d028f6628465234b48e54be8c813
bardiamadadi86-afk <bardiamadadi86@gmail.com>
2026-06-04 10:58:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://rzxhhokujxzksjlmarjp.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6eGhob2t1anh6a3NqbG1hcmpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NjkwOTgsImV4cCI6MjA5NjA0NTA5OH0.R38hwxo-5nMP-duxoulgTs3PKY2IaAPWikSwJmtlpcA";
+const SUPABASE_KEY = "key";
 const supabaseClient  = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
